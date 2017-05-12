fed0f0e24f660f8e7e01952edb9bedcbe3f5a428
kevin <kevinqjin@gmail.com>
2017-05-12 13:08:46 -0500

    updated package name, switch turn and insert

--- src/connect_four.rs
+++ src/connect_four.rs
@@ -1,5 +1,8 @@
 use std::fmt;
 
+const player_1: usize = 0;
+const player_2: usize = 1;
+
 pub struct connect_four {
 	board: Vec<Vec<usize>>,
 	player_1_turn: bool,
@@ -17,6 +20,24 @@ impl connect_four {
 		}
 	}
 
+	pub fn insert(&mut self, col: usize) -> Result<(), &'static str> {
+		// if isValidMove(col) {
+		if col <= 6 {
+			if self.player_1_turn {
+				self.board[col].push(player_1);
+			} else {
+				self.board[col].push(player_2);
+			}
+			self.change_turn();
+			Ok(())
+		} else {
+			Err("invalid move")
+		}
+	}
+
+	fn change_turn(&mut self) {
+		self.player_1_turn = !self.player_1_turn;
+	}
 	//is game done - Jeanette
 	//insert - Kevin
 	//switch player (concurrency) - Kevin
@@ -25,10 +46,10 @@ impl connect_four {
 
 
 //David
-impl fmt::Debug for connect_four {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		for _x in 0..7 {
-			write!(f, "{}", self.board);
-		}
-    }
-}
+// impl fmt::Debug for connect_four {
+// 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+// 		for _x in 0..7 {
+// 			write!(f, "{}", self.board);
+// 		}
+//     }
+// }
