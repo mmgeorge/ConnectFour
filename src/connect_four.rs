92b513ae8b7d1a6f2e886a70bfe3695b3d658a83
kevin <kevinqjin@gmail.com>
2017-05-12 13:28:03 -0500

    changed connect_four board implementation to prepopulated array

--- src/connect_four.rs
+++ src/connect_four.rs
@@ -1,39 +1,39 @@
 use std::fmt;
 
-const player_1: usize = 0;
-const player_2: usize = 1;
+const PLAYER_1: usize = 1;
+const PLAYER_2: usize = 2;
+const WIDTH   : usize = 7;
+const HEIGHT  : usize = 6;
+
 
 pub struct connect_four {
-	board: Vec<Vec<usize>>,
+	board: [[usize; HEIGHT]; WIDTH],
 	player_1_turn: bool,
 }
 
 impl connect_four {
 	pub fn new() -> Self {
-		let mut new_board = Vec::new();
-		for _x in 0..7 {
-			new_board.push(Vec::new());
-		}
+		let new_board = [[0; HEIGHT]; WIDTH];
 		connect_four {
 			board: new_board,
 			player_1_turn: true,
 		}
 	}
 
-	pub fn insert(&mut self, col: usize) -> Result<(), &'static str> {
-		// if isValidMove(col) {
-		if col <= 6 {
-			if self.player_1_turn {
-				self.board[col].push(player_1);
-			} else {
-				self.board[col].push(player_2);
-			}
-			self.change_turn();
-			Ok(())
-		} else {
-			Err("invalid move")
-		}
-	}
+	// pub fn insert(&mut self, col: usize) -> Result<(), &'static str> {
+	// 	// if isValidMove(col) {
+	// 	if col <= 6 {
+	// 		if self.player_1_turn {
+	// 			self.board[col](PLAYER_1);
+	// 		} else {
+	// 			self.board[col].push(PLAYER_2);
+	// 		}
+	// 		self.change_turn();
+	// 		Ok(())
+	// 	} else {
+	// 		Err("invalid move")
+	// 	}
+	// }
 
 	fn change_turn(&mut self) {
 		self.player_1_turn = !self.player_1_turn;
@@ -44,6 +44,19 @@ impl connect_four {
 	//isValidMove (check if col is full or col is between 0-7 exclusive) - David
 }
 
+
+#[cfg(test)]
+mod insert_test {
+	use super::connect_four;
+	#[test] 
+	fn column_too_high() {
+		
+	}
+
+	fn validate_insert() {
+		let mut game = connect_four::new();
+	}
+}
 
 //David
 // impl fmt::Debug for connect_four {
